baa3141100169740ed845d6be04ff886b1c4c3ad
FarhaFuard <s22010470@ousl.lk>
2024-12-14 20:42:10 +0530

    Update scores.sql

--- TinyGenius - Frontend/scores.sql
+++ TinyGenius - Frontend/scores.sql
@@ -24,16 +24,15 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `progress`
+-- Table structure for table `scores`
 --
 
-CREATE TABLE `progress` (
-  `progress_id` int(11) NOT NULL,
+CREATE TABLE `scores` (
+  `score_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `subject` varchar(50) NOT NULL,
-  `completed_quizzes` int(11) NOT NULL,
-  `total_quizzes` int(11) NOT NULL,
-  `progress_percentage` decimal(5,2) NOT NULL
+  `quiz_id` int(11) NOT NULL,
+  `score` int(11) NOT NULL,
+  `completed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -41,34 +40,35 @@ CREATE TABLE `progress` (
 --
 
 --
--- Indexes for table `progress`
+-- Indexes for table `scores`
 --
-ALTER TABLE `progress`
-  ADD PRIMARY KEY (`progress_id`),
-  ADD KEY `user_id` (`user_id`);
+ALTER TABLE `scores`
+  ADD PRIMARY KEY (`score_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `quiz_id` (`quiz_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `progress`
+-- AUTO_INCREMENT for table `scores`
 --
-ALTER TABLE `progress`
-  MODIFY `progress_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `scores`
+  MODIFY `score_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `progress`
+-- Constraints for table `scores`
 --
-ALTER TABLE `progress`
-  ADD CONSTRAINT `progress_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+ALTER TABLE `scores`
+  ADD CONSTRAINT `scores_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `scores_ibfk_2` FOREIGN KEY (`quiz_id`) REFERENCES `quizzes` (`quiz_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
